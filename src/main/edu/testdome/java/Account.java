805daddeb01b014a6c641ed9947672fb0492c529
HimashiNethinikaRodrigo <himashirodrigo23@gmail.com>
2020-06-22 17:02:31 +0200

    account class

---
+++ src/main/edu/testdome/java/Account.java
@@ -0,0 +1,35 @@
+package main.edu.testdome.java;
+
+public class Account {
+    private double balance;
+    private double overdraftLimit;
+
+    public Account(double overdraftLimit) {
+        this.balance = 0;
+        this.overdraftLimit = overdraftLimit > 0 ? overdraftLimit : 0;
+    }
+
+    public boolean deposit(double amount) {
+        if(amount >= 0) {
+            this.balance += amount;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean withdraw(double amount) {
+        if(this.balance - amount >= -this.overdraftLimit && amount >= 0) {
+            this.balance -= amount;
+            return true;
+        }
+        return false;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public double getOverdraftLimit() {
+        return overdraftLimit;
+    }
+}
