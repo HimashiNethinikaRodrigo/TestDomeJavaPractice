2b73024bc29797d324f48ed072b7d01f95e80b3a
HimashiNethinikaRodrigo <himashirodrigo23@gmail.com>
2020-06-23 22:06:08 +0200

    add pass rate

--- src/main/edu/testdome/java/sortedsearch/SortedSearchEfficient.java
+++ src/main/edu/testdome/java/sortedsearch/SortedSearchEfficient.java
@@ -1,5 +1,14 @@
 package main.edu.testdome.java.sortedsearch;
+/*
+Implement function countNumbers that accepts a sorted array of unique integers and,
+efficiently with respect to time used,
+counts the number of array elements that are less than the parameter lessThan.
 
+For example, SortedSearch.countNumbers(new int[] { 1, 3, 5, 7 }, 4)
+should return 2 because there are two array elements less than 4.
+
+Test case pass rate: 75%
+ */
 public class SortedSearchEfficient {
     public static int countNumbers(int[] sortedArray, int lessThan) {
         if (sortedArray.length <= 1 || lessThan < sortedArray[0]) return 0;
--- src/main/edu/testdome/java/sortedsearch/SortedSearchRecursive.java
+++ src/main/edu/testdome/java/sortedsearch/SortedSearchRecursive.java
@@ -1,5 +1,14 @@
 package main.edu.testdome.java.sortedsearch;
+/*
+Implement function countNumbers that accepts a sorted array of unique integers and,
+efficiently with respect to time used,
+counts the number of array elements that are less than the parameter lessThan.
 
+For example, SortedSearch.countNumbers(new int[] { 1, 3, 5, 7 }, 4)
+should return 2 because there are two array elements less than 4.
+
+Test case pass rate: 100%
+ */
 public class SortedSearchRecursive {
     public static int countNumbers(int[] sortedArray,int left, int right, int lessThan) {
         int middle = left + (right - left) / 2;
